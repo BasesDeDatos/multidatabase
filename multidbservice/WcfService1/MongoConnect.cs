b3336fb11d3ea7c271ede641e54ff126feb0c88b
Jeffrey Alvarado <jefalva10296@gmail.com>
2016-04-04 20:33:00 -0600

    Se crea MongoConnect.cs

---
+++ multidbservice/WcfService1/MongoConnect.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nsMultiDBService
+{
+    public class MongoConnect
+    {
+        private IMongoClient client;
+        private IMongoDatabase database;
+
+        public MongoConnect(string pDatabase)
+        {
+            client = new MongoClient();
+            database = client.GetDatabase(pDatabase);
+        }
+
+        public async void Select(string table)
+        {
+            var collection = database.GetCollection<BsonDocument>(table);
+            var filter = new BsonDocument();
+            var count = 0;
+            ArrayList list = new ArrayList();
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        list.Add(document);
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
